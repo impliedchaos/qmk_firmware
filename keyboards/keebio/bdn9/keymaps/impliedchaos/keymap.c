92e718afe7eaf4c73c8597c5a1a605bda940cfd7
impliedchaos <crap@dharmadevil.com>
2021-12-09 05:02:56 -0500

    ok

---
+++ keyboards/keebio/bdn9/keymaps/impliedchaos/keymap.c
@@ -0,0 +1,40 @@
+/* Copyright 2019 Danny Nguyen <danny@keeb.io>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+        | Knob 1: Vol Dn/Up |      | Knob 2: Page Dn/Up |
+        | Press: Mute       | Home | Press: Play/Pause  |
+        | Hold: Layer 2     | Up   | RGB Mode           |
+        | Left              | Down | Right              |
+     */
+    [0] = LAYOUT(
+        LT(1,LCTL(KC_F13)), LCTL(KC_F14), LCTL(KC_F15),
+        LCTL(KC_F16), LCTL(KC_F17), LCTL(KC_F18),
+        KC_MPRV, KC_MPLY, KC_MNXT
+    ),
+    /*
+        | RESET          | N/A  | Media Stop |
+        | Held: Layer 2  | Home | RGB Mode   |
+        | Media Previous | End  | Media Next |
+     */
+    [1] = LAYOUT(
+        _______, LCTL(KC_F19), LCTL(KC_F20),
+        RESET,   LCTL(KC_F22), LCTL(KC_F23),
+        BL_STEP, LCTL(KC_F21), LCTL(KC_F24)
+    ),
+};
